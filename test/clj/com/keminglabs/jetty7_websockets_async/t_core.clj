5a7b3f1720df298d0837f7b65247c748d485076c
Kevin J. Lynagh and Zach Allaun <kevin@mu.local>
2013-08-05 18:19:26 -0400

    Add send/recv tests.

--- test/clj/com/keminglabs/jetty7_websockets_async/t_core.clj
+++ test/clj/com/keminglabs/jetty7_websockets_async/t_core.clj
@@ -4,7 +4,7 @@
             [clojure.core.async :refer [go close! >!! chan timeout alt!!]]
             clojure.core.async.impl.protocols
             [midje.sweet :refer :all])
-  (:import (org.eclipse.jetty.websocket WebSocketClientFactory WebSocket)
+  (:import (org.eclipse.jetty.websocket WebSocketClientFactory WebSocket WebSocket$OnTextMessage)
            java.net.URI))
 
 (defn <!!
@@ -45,4 +45,32 @@ Why is this not in core.async, yo?"
                 uri => "/"
                 conn => #(instance? org.eclipse.jetty.websocket.WebSocket$Connection %)
                 send => #(satisfies? clojure.core.async.impl.protocols/WritePort %)
-                recv => #(satisfies? clojure.core.async.impl.protocols/ReadPort %)))))
+                recv => #(satisfies? clojure.core.async.impl.protocols/ReadPort %)))
+
+        (fact "Sending to client"
+              (let [received-messages (chan)]
+
+                (.open client (URI. (str "ws://localhost:" test-port))
+                       (proxy [WebSocket$OnTextMessage] []
+                         (onOpen [conn])
+                         (onClose [close-code msg])
+                         (onMessage [msg]
+                           (>!! received-messages msg))))
+
+                (let [test-message "test-message"
+                      {:keys [send]} (<!! new-connections 100 :fail)]
+                  (>!! send test-message)
+                  (<!! received-messages 100 :fail) => test-message)))
+
+
+        (fact "Receive from client"
+              (let [test-message "test-message"]
+                (.open client (URI. (str "ws://localhost:" test-port))
+                       (proxy [WebSocket$OnTextMessage] []
+                         (onOpen [conn]
+                           (.sendMessage conn test-message))
+                         (onClose [close-code msg])
+                         (onMessage [msg])))
+
+                (let [{:keys [recv]} (<!! new-connections 100 :fail)]
+                  (<!! recv 100 :fail) => test-message)))))
